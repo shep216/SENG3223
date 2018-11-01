e6af36f3156acefd397746e22f43fdc405f004f6
DESKTOP-SC6I7KB\prest <preston.grau@gmail.com>
2018-11-01 14:58:09 -0500

    A couple of new scenes

---
+++ Proj - Concierge Elite/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour {
+
+    void Login()
+    {
+
+    }
+
+    void SignUp()
+    {
+
+    }
+
+    void NextScene()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
